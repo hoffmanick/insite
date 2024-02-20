53a56e688d3b2be2cb416c4a42ef85b4aa7a8165
hoffmanick <134728053+hoffmanick@users.noreply.github.com>
2024-02-20 15:48:51 -0800

    Create phpTEST.php

---
+++ phpTEST.php
@@ -0,0 +1,10 @@
+<?php
+
+$spec =$_POST['species'];
+$date = $_POST['trip-start'];
+$lat = $_POST['lat'];
+$long = $_POST['long'];
+$store = $_POST['store'];
+
+echo $spec. "<br>". $date."<br>".$lat."<br>".$long."<br>".$store."<br>";
+?>
