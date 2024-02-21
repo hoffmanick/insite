b045ff5b2c1d2c84b6ce1defa3d0eb5722d6c9aa
hoffmanick <134728053+hoffmanick@users.noreply.github.com>
2024-02-20 16:07:23 -0800

    Update phpTEST.php

--- phpTEST.php
+++ phpTEST.php
@@ -6,5 +6,25 @@ $lat = $_POST['lat'];
 $long = $_POST['long'];
 $store = $_POST['store'];
 
-echo $spec. "<br>". $date."<br>".$lat."<br>".$long."<br>".$store."<br>";
+$servername = "localhost";
+$username = "root";
+$password = "insite_data";
+$db = "mydb";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error){
+        die("Connection failed: ". $conn->connect_error);
+}
+
+  $sql = "insert into specimen(species,date,lat,long,store)
+    values('$spec','$date','$lat','$long','$store')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "ADDED: ".$species.", ".$date.", ".$lat.", ".$long.", ".$store.";
+} else {
+    echo "Error: ".$sql."<br>".$conn->error;
+    }
+
+    $conn-> close();
 ?>
